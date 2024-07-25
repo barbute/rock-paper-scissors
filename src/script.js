68cde3842abe6c840b8371ff2edb54ce30b9ea46
barbute <jackhdemeterio@gmail.com>
2024-07-25 18:55:06 -0500

    Add getComputerChoice function to get a pseduo-random choice from the computer

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+/*
+ * Script for a Rock-Paper-Scissors game
+ * ---
+ * Author: barbute
+ */
+
+// -1 will indicate no choice or invalid choice throughout this program
+let computerChoice = -1;
+let playerChoice = -1;
+
+function getComputerChoice() {
+    computerChoice = Math.floor(Math.random() * 3);
+
+    return computerChoice;
+}
